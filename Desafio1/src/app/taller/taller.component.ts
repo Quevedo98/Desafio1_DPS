375def0316200fd6592972f152df3cf0f860d7a7
Quevedo98 <oscarqp1@gmail.com>
2020-08-29 18:14:54 -0600

    Primera mitad

---
+++ Desafio1/src/app/taller/taller.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule}from '@angular/platform-browser'
+
+
+@Component({
+  selector: 'app-taller',
+  templateUrl: './taller.component.html',
+  styleUrls: ['./taller.component.css']
+})
+export class TallerComponent implements OnInit {
+
+   arrayRegistro = [];
+  cliente:any;
+  nombre:string;
+  DUI:string;
+  vehic:string;
+  costo:number;
+  descCosto:number;
+  ticket:boolean = false;
+  cuenta:number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.nombre='';
+    this.DUI='';
+    this.vehic='';
+    this.costo=0;
+  }
+
+   registrar(){//guarda los datos del cliente y cuenta las visitas para aplicar el descuento
+    this.cliente={"nombre":this.nombre,"DUI":this.DUI,"vehicu":this.vehic,"cost":this.costo};     
+     this.cuenta = this.contVisita();   
+      if(this.cuenta == 2){
+        this.descCosto = this.costo - (this.costo * 0.05);        
+      }
+      else if(this.cuenta > 4){
+        this.descCosto = this.costo - (this.costo * 0.10);        
+      }
+      else{
+        this.descCosto = 0;
+      }
+    this.arrayRegistro.push(this.cliente);
+    this.ticket = true;
+  }
+  contVisita():number{//Cuenta las visitas segun el dui
+    let n = 0
+    for (let clie of this.arrayRegistro){      
+      if(clie.DUI==this.DUI){
+        n++;        
+      }
+    }
+    return n    
+  }
+}
+
