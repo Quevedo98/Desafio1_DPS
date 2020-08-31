06d2c6196e28b0a9f81c0aeb89c58e262b47c4fe
luigichevez19 <luigichevez@gmail.com>
2020-08-30 20:47:17 -0600

    Final del desafio

--- Desafio1/src/app/taller/taller.component.ts
+++ Desafio1/src/app/taller/taller.component.ts
@@ -19,7 +19,7 @@ export class TallerComponent implements OnInit {
   descCosto:number;
   ticket:boolean = false;//Para mostrar el ticket y esconder el form y viceversa
   cuenta:number;
-
+  descu:number;
   constructor() { }
 
   ngOnInit(): void {
@@ -27,31 +27,45 @@ export class TallerComponent implements OnInit {
     this.DUI='';
     this.vehic='';
     this.costo=0;
+    this.descu=0;
   }
 
    registrar(){//guarda los datos del cliente y cuenta las visitas para aplicar el descuento
     this.cliente={"nombre":this.nombre,"DUI":this.DUI,"vehicu":this.vehic,"cost":this.costo};     
      this.cuenta = this.contVisita();//Cuenta las visitas  
       if(this.cuenta == 2){
-        this.descCosto = this.costo - (this.costo * 0.05);        
+        this.descu=(this.costo * 0.05);
+        this.descCosto = this.costo - this.descu ;        
       }
       else if(this.cuenta > 4){
-        this.descCosto = this.costo - (this.costo * 0.10);        
+        this.descu=(this.costo * 0.10);
+        this.descCosto = this.costo - this.descu ;       
       }
       else{
+        this.descu=0;
         this.descCosto = 0;
+        this.descCosto=this.costo;
       }
+    this.descCosto.toString(3);
     this.arrayRegistro.push(this.cliente);//Agrega el registro al arreglo
     this.ticket = true;
   }
   contVisita():number{//Cuenta las visitas segun el dui
-    let n = 0
+    let n = 1
     for (let clie of this.arrayRegistro){      
       if(clie.DUI==this.DUI){
         n++;        
       }
     }
     return n    
+  }
+  vaciar()
+  {
+    this.nombre="";
+    this.DUI="";
+    this.vehic="";
+    this.costo=0;
+    this.ticket=false;
   }
 }
 
